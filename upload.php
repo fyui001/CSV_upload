2ceedb878afe475390732e4a90d4a59dcf134f08
Yui501 <yun.cod@gmail.com>
2018-11-08 13:03:33 +0900

    commit

--- upload.php
+++ upload.php
@@ -1,9 +1,9 @@
 <?php
 /* データベース設定諸々 */
-$db_name = 'DB_name';
+$db_name = 'JapanAddress';
 $host = 'localhost';
-$usr = 'user_name';
-$passwd = 'password';
+$usr = 'root';
+$passwd = 'hogehoge';
 function getDb($db_name, $host, $usr, $passwd){
   try{
     $db = new PDO("mysql:dbname={$db_name}; host={$host}; charset=utf8;", $usr, $passwd);
@@ -16,7 +16,7 @@ function getDb($db_name, $host, $usr, $passwd){
 }
 
 /* テーブル名指定*/
-$table_name = 'TableName';
+$table_name = 'PostAddress';
 
 /* テーブルの存在確認 */
 $db = getDb($db_name, $host, $usr, $passwd);
@@ -27,64 +27,73 @@ if(empty($tables)){
   echo 'テーブルが存在しないので作成：テーブル名'.$table_name."\n";
   $stt = $db->prepare("
   CREATE TABLE `{$table_name}` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `PostNum` char(7) DEFAULT NULL,
-  `PrefectureKana` varchar(10) DEFAULT NULL,
-  `CityKana` varchar(100) DEFAULT NULL,
-  `TownKana` varchar(100) DEFAULT NULL,
-  `Prefecture` varchar(10) DEFAULT NULL,
-  `City` varchar(200) DEFAULT NULL,
-  `Town` varchar(200) DEFAULT NULL,
-  `systime` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=124251 DEFAULT CHARSET=utf8;
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `PostNum` char(7) DEFAULT NULL,
+    `PrefectureKana` varchar(10) DEFAULT NULL,
+    `CityKana` varchar(100) DEFAULT NULL,
+    `TownKana` varchar(100) DEFAULT NULL,
+    `Prefecture` varchar(10) DEFAULT NULL,
+    `City` varchar(200) DEFAULT NULL,
+    `Town` varchar(200) DEFAULT NULL,
+    `systime` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=124251 DEFAULT CHARSET=utf8;
     ");
     $stt->execute();
 
   }
 
-/* 大元のファイルパスとタイムスタンプ */
-$filePath = "FilePath";
-$time =  date("YmdHis");
+  /* 大元のファイルパスとタイムスタンプ */
+  $filePath = "/var/www/html/upload/UploadFiles/";
+  $time =  date("YmdHis");
 
-//ディレクトリの存在確認
-if(!file_exists("{$filePath}")){
-  mkdir("{$filePath}");
-}
+  //ディレクトリの存在確認
+  if(!file_exists("{$filePath}")){
+    mkdir("{$filePath}");
+  }
 
-/*  ファイル名変更 */
-$FileName = $_FILES['file']['name'];
-$newFileName = $time.$FileName;
-$UploadPath =  $filePath.$newFileName;
+  /*  ファイル名変更 */
+  $FileName = $_FILES['file']['name'];
+  $newFileName = $time.$FileName;
+  $UploadPath =  $filePath.$newFileName;
 
-if (move_uploaded_file($_FILES["file"]["tmp_name"], $UploadPath)){
-  // ファイルアップロード成功
-  echo "OK";
-}else{
-  // ファイルアップロード失敗
-  echo "失敗";
-}
+  if (move_uploaded_file($_FILES["file"]["tmp_name"], $UploadPath)){
+    // ファイルアップロード成功
+    echo "OK";
+  }else{
+    // ファイルアップロード失敗
+    echo "失敗";
+  }
 
-/* 文字コードの変更と諸々 */
-$exe = 'iconv -f Shift_JIS -t UTF8 '.$UploadPath.' |';
-$exe .= "awk -v RS='\r\n' '".'BEGIN {FS=",";OFS=","}{print$3,$4,$5,$6,$7,$8,$9,"\""strftime("%Y-%m-%d %H:%M:%S",systime())"\""}'."' > ";
-$encfileName = $newFileName."encode.csv";
-$encFilePath = $filePath.$encfileName;
-$exe .= $encFilePath;
-exec($exe);
+  /* 文字コードの変更と諸々 */
+  $exe = 'iconv -f Shift_JIS -t UTF8 '.$UploadPath.' |';
+  $exe .= "awk -v RS='\r\n' '".'BEGIN {FS=",";OFS=","}{print$3,$4,$5,$6,$7,$8,$9,"\""strftime("%Y-%m-%d %H:%M:%S",systime())"\""}'."' > ";
+  $encfileName = $newFileName."encode.csv";
+  $encFilePath = $filePath.$encfileName;
+  $exe .= $encFilePath;
+  exec($exe);
 
-/* sql文の存在確認 */
-$sqlFileName = "{$newFileName}.sql";
-$sqlFilePath = "{$filePath}{$sqlFileName}";
-if(!file_exists("{$sqlFilePath}")){
-  touch("{$sqlFilePath}");
-  $a = fopen("$sqlFilePath", "w");
-  @fwrite($a, " truncate table `{$table_name}`; LOAD DATA LOCAL INFILE '{$encFilePath}' INTO TABLE `{$table_name}` FIELDS TERMINATED BY ',' ENCLOSED BY '".'"'."' (PostNum, PrefectureKana, CityKana, TownKana, Prefecture, City, Town, systime); ");
-  fclose($a);
-}
+  /* sql文の存在確認 */
+  $sqlFileName = "{$newFileName}.sql";
+  $sqlFilePath = "{$filePath}{$sqlFileName}";
+  if(!file_exists("{$sqlFilePath}")){
+    touch("{$sqlFilePath}");
+    $a = fopen("$sqlFilePath", "w");
+    if($a == null){
+      echo "やっちまった。";
+      exit;
+    }
+    $r = fwrite($a, " truncate table `{$table_name}`; LOAD DATA LOCAL INFILE '{$encFilePath}' INTO TABLE `{$table_name}` FIELDS TERMINATED BY ',' ENCLOSED BY '".'"'."' (PostNum, PrefectureKana, CityKana, TownKana, Prefecture, City, Town, systime); ");
+    if(!$r){
+      echo "やっちまった。";
+      exit;
+    }
+    fclose($a);
 
-/* DBインポートするsqlファイルの実行 */
-$exe = " mysql -u {$usr} -p{$passwd} {$db_name} < {$sqlFilePath} ";
+  }
 
-exec($exe);
-exit;
+  /* DBインポートするsqlファイルの実行 */
+  $exe = " mysql -u {$usr} -p{$passwd} {$db_name} < {$sqlFilePath} ";
+
+  exec($exe);
+  exit;
